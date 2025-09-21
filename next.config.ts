d96c4ca2c528cc8abb816935e347936c18bb74aa
joseph-vazquez <josephvschool0469@gmail.com>
2025-09-20 22:28:00 -0700

    FireBase mega update upload

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   trailingSlash: true,
   images: {
     unoptimized: true,
     domains: ['media1.giphy.com', 'media3.giphy.com'],
   },
   output: 'export',
-  distDir: 'dist',
-  assetPrefix: '',
-  // For Firebase hosting
-  experimental: {
-    appDir: true,
-  }
+  distDir: 'out'
 }
 
 module.exports = nextConfig
